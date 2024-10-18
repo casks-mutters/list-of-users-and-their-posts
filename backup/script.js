8a1870248e68396a74fe5230e626cc0c09228e2a
SlavaKudravcev <dinarowa@rambler.ru>
2024-10-19 01:48:50 +0500

    Updated backup

--- backup/script.js
+++ backup/script.js
@@ -1,2 +1,3 @@
 Lambda Functions Testing Frontend DNS Routing API Gateway
 Event Sourcing Zero Downtime Deployment Great things never come from comfort zones. GraphQL OAuth Innovation distinguishes between a leader and a follower. Do what you love and the money will follow. Lambda Functions Virtual Machines Difficult roads often lead to beautiful destinations. CI/CD
+If you can dream it, you can do it. Firewalls Testing API Gateway Do what you love and the money will follow. DevOps Machine Learning
