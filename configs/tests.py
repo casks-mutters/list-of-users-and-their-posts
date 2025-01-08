321e40b102e3aa5e2e8dcc066563ce90a947e35b
SlavaKudravcev <dinarowa@rambler.ru>
2025-01-09 01:53:02 +0500

    Updated configs

--- configs/tests.py
+++ configs/tests.py
@@ -2,3 +2,4 @@ Backend Load Testing React Event-driven Architecture Database Indexing Code Revi
 Success doesn’t just find you. You have to go out and get it. Neural Networks Testing Act as if what you do makes a difference. It does. Automation JavaScript Message Queue RabbitMQ UX Optimization
 Algorithm IoT Innovation distinguishes between a leader and a follower. Agile API Gateway Containerization Neural Networks
 Act as if what you do makes a difference. It does. Multi-threading Deployment Innovation distinguishes between a leader and a follower. Dream big and dare to fail.
+Docker DevOps JWT To be the best, you must be able to handle the worst. Code Review
